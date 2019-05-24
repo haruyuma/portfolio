4367412509d20dfd765aa466791a9473e633b20c
haruyuma <tomo.tatsumix@gmail.com>
2019-05-24 23:19:10 +0900

    2019/5/24-2

--- contact.php
+++ contact.php
@@ -1,20 +1,48 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8" />
-  </head>
-  <body>
 <?php
-     mb_language("Japanese");
-     mb_internal_encoding("UTF-8");
-     $to = $_POST['email'];
-     $title = $_POST['subject'];
-     $content = $_POST['message'];
-     if(mb_send_mail($to, $title, $content)){
-       echo "メールを送信しました";
-     } else {
-       echo "メールの送信に失敗しました";
-     };
-   ?>
- </body>
- </html>
+
+// configure
+$from = "tomo.tatsumix@gmail.com";
+$sendTo = "tomo.tatsumix@gmail.com"; // Add Your Email
+$subject = "New message from contact form";
+$fields = array('name' => 'Name', 'subject' => 'Subject', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in the email
+$okMessage = "Contact form successfully submitted. Thank you, I will get back to you soon!"";
+$errorMessage = "There was an error while submitting the form. Please try again later";
+
+// let's do the sending
+
+try
+{
+    $emailText = "You have new message from contact form\n=============================\n";
+
+    foreach ($_POST as $key => $value) {
+
+        if (isset($fields[$key])) {
+            $emailText .= "$fields[$key]: $value\n";
+        }
+    }
+
+    $headers = array('Content-Type: text/plain; charset="UTF-8";',
+        'From: ' . $from,
+        'Reply-To: ' . $from,
+        'Return-Path: ' . $from,
+    );
+
+    mail($sendTo, $subject, $emailText, implode("\n", $headers));
+
+    $responseArray = array('type' => 'success', 'message' => $okMessage);
+}
+catch (\Exception $e)
+{
+    $responseArray = array('type' => 'danger', 'message' => $errorMessage);
+}
+
+if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+    $encoded = json_encode($responseArray);
+
+    header('Content-Type: application/json');
+
+    echo $encoded;
+}
+else {
+    echo $responseArray['message'];
+}
